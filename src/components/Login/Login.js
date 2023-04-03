a6640076747dbd5f7af164b5a2769797de75bc83
Evan McGrath <evan.grath@gmail.com>
2023-04-03 17:21:11 -0400

    Fix header and footer to top and bottom of page respectively

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -7,8 +7,8 @@ const Login = () => {
 
     return (
         <div className="login">
+            <h2 className="login__subheader">Start your jam session.</h2>
             <Link className="login__button" to='http://localhost:3100/login' >Login</Link>
-
         </div>
     )
 }
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,13 +1,17 @@
+import './LandingPage.scss'
 import Login from '../components/Login/Login'
 import Dashboard from '../components/Dashboard/Dashboard'
 
 const LandingPage = ({ code }) => {
 
     return (
-        <>
+        <main className="landing">
+            <header className="landing__header">Jamify</header>
             {code ? <Dashboard code={code} /> : <Login />}
-            <div>Hiya.</div>
-        </>
+            <footer className="landing__footer">
+                <div>Logo</div>
+            </footer>
+        </main>
     )
 }
 
