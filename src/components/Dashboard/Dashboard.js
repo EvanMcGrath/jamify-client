59b2b60fa8ac0840a2b765a35ae87110c68e8d0e
Evan McGrath <evan.grath@gmail.com>
2023-04-03 22:29:35 -0400

    Set up useEffect to fetch userId from spotifyAPI

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,17 +1,43 @@
 import React from "react";
 import useAuth from "../../utils/useAuth";
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const Dashboard = ({ code }) => {
 
+    const [myInfo, setMyInfo] = useState();
     const accessToken = useAuth(code)
 
+
+    useEffect(() => {
+        if (!accessToken) return
+        axios.post('http://localhost:3100/me', {
+            "access_token": `${accessToken}`
+        })
+            .then((data) => {
+                console.log(data)
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    }, [accessToken])
+
+    
     return (
         <>
-            <div>{code}</div>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+
+
+            {/* <div>{code}</div>
             <br /> 
             <br /> 
-            <br />
-            <div>{accessToken ? accessToken : "null"}</div>
+            <br /> */}
+            <div>{accessToken ? accessToken : "null"}</div> */}
         </>
     )
 }
--- src/pages/PlaylistMenu/PlaylistMenu.js
+++ src/pages/PlaylistMenu/PlaylistMenu.js
@@ -1,4 +1,5 @@
 import Dashboard from '../../components/Dashboard/Dashboard'
+import './PlaylistMenu.scss'
 
 const PlaylistMenu = ({ code }) => {
 
@@ -6,9 +7,15 @@ const PlaylistMenu = ({ code }) => {
 
     return (
         <>
+            <header className="menu-header"> 
+                <h1 className="menu-header__title">
+                    Playlists
+                </h1>
+                <div className="menu-header__logo">Logo</div>
+            </header>
             <Dashboard code={code} />
         </>
     )
 }
 
-export default Dashboard;
+export default PlaylistMenu;
